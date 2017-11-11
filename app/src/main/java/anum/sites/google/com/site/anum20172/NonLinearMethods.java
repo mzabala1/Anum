28e7cdd785735d35f5b59150f48b3232950f6911
smendoz3 <smendoz3@eafit.edu.co>
2017-11-11 16:15:05 -0500

    Non linear methods UI :v

--- app/src/main/java/anum/sites/google/com/site/anum20172/NonLinearMethods.java
+++ app/src/main/java/anum/sites/google/com/site/anum20172/NonLinearMethods.java
@@ -1,19 +1,29 @@
 package anum.sites.google.com.site.anum20172;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class NonLinearMethods extends AppCompatActivity {
+public class NonLinearMethods extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnIncSearch, btnFixedPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_linear_methods);
 
-        /*btnBisection = findViewById(R.id.btnBisection);
         btnIncSearch = findViewById(R.id.btnIncSearch);
         btnFixedPoint = findViewById(R.id.btnFixedPoint);
-        btnMultSQroot = findViewById(R.id.btnMultSQroot);
+
+        btnIncSearch.setOnClickListener(this);
+        btnFixedPoint.setOnClickListener(this);
+
+        //btnBisection = findViewById(R.id.btnBisection);
+
+        /*btnMultSQroot = findViewById(R.id.btnMultSQroot);
         btnFalseRule = findViewById(R.id.btnFalseRule);
         btnSecant = findViewById(R.id.btnSecant);
         btnNewton = findViewById(R.id.btnNewton);
@@ -25,5 +35,21 @@ public class NonLinearMethods extends AppCompatActivity {
         btnFalseRule.setOnClickListener(this);
         btnSecant.setOnClickListener(this);
         btnNewton.setOnClickListener(this);*/
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnIncSearch:
+                Intent aInc = new Intent(NonLinearMethods.this, IncSearch.class);
+                startActivity(aInc);
+                finish();
+                break;
+            case R.id.btnFixedPoint:
+                Intent aFp = new Intent(NonLinearMethods.this, FixedPoint.class);
+                startActivity(aFp);
+                finish();
+                break;
+        }
     }
 }
