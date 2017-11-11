0cf7ab297519c7834b56f1ec994c632355105ad6
Juan Felipe Donado <pipe.donado@gmail.com>
2017-11-11 15:02:52 -0500

    Bisection done

--- app/src/main/java/anum/sites/google/com/site/anum20172/InterpolationMethods.java
+++ app/src/main/java/anum/sites/google/com/site/anum20172/InterpolationMethods.java
@@ -20,22 +20,6 @@ public class InterpolationMethods extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        /*btnBisection = findViewById(R.id.btnBisection);
-        btnIncSearch = findViewById(R.id.btnIncSearch);
-        btnFixedPoint = findViewById(R.id.btnFixedPoint);
-        btnMultSQroot = findViewById(R.id.btnMultSQroot);
-        btnFalseRule = findViewById(R.id.btnFalseRule);
-        btnSecant = findViewById(R.id.btnSecant);
-        btnNewton = findViewById(R.id.btnNewton);
-
-        btnBisection.setOnClickListener(this);
-        btnIncSearch.setOnClickListener(this);
-        btnFixedPoint.setOnClickListener(this);
-        btnMultSQroot.setOnClickListener(this);
-        btnFalseRule.setOnClickListener(this);
-        btnSecant.setOnClickListener(this);
-        btnNewton.setOnClickListener(this);*/
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interpolation_methods);
     }
